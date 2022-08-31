3f4026bb45ebf7fedf5a0473894629d3450342bf
SamuelGzz95 <samuel_gzz_95@hotmail.com>
2022-08-31 14:22:40 -0500

    2 files modified, 8 added

--- src/app/modules/Home/home.module.ts
+++ src/app/modules/Home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './public/components/pages/login/login.component';
+import { HomeComponent } from './public/components/pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
